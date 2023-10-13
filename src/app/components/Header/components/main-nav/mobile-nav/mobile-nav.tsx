c352792a65dfc722e521459ab7c4c10c0e710a4a
mariquegonn-dev <ocatodev@gmail.com>
2023-10-13 14:05:17 -0300

    style: css enviroments

--- src/app/components/Header/components/main-nav/mobile-nav/mobile-nav.tsx
+++ src/app/components/Header/components/main-nav/mobile-nav/mobile-nav.tsx
@@ -6,10 +6,7 @@ import Link from 'next/link'
 
 export const MobileNavLink = ({ title, href, icon }: HeaderType) => {
   return (
-    <Link
-      className="text-lg p-3 inline-block text-white-primary p-6"
-      href={href}
-    >
+    <Link className="text-lg grid text-white-primary p-6" href={href}>
       <span
         className={cn(
           ' py-1 transition-colors hover:text-purple-primary',
