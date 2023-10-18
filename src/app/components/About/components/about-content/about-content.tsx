9aef9d537b4ce7816661434ebb18184782e41650
mariquegonn-dev <ocatodev@gmail.com>
2023-10-18 19:40:52 -0300

    styles: removed detail in paragraph

--- src/app/components/About/components/about-content/about-content.tsx
+++ src/app/components/About/components/about-content/about-content.tsx
@@ -9,10 +9,7 @@ export const AboutContent = ({ items }: AboutContentProps) => {
     <div className="grid gap-6">
       <h1 className="text-3xl font-bold">Sobre mim</h1>
       {items.items.map((item, index) => (
-        <p className="relative" key={index}>
-          <span className='before:content-[""] before:w-6 before:h-2 before:bg-blue-primary before:absolute before:-top-4 after:content-[""] after:w-10 after:h-2 after:bg-blue-secundary after:absolute after:-top-2'></span>
-          {item.desc}
-        </p>
+        <p key={index}>{item.desc}</p>
       ))}
     </div>
   )
